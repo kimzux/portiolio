3d75194b44f1fa9e80db4db3993be839309bd6b3
kimzux <kimzux0718@gmail.com>
2023-01-15 23:39:31 +0300

    portifolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
             <div>
                 <img src={HeroImage} alt="my profile" className="rounded-2xl mt-[10px] mx-auto w-2/3 md:w-full " />
             </div>
-                <div className='flex flex-col justify-center mt-2 h-full ml-2'>
+                <div className='flex flex-col justify-center h-full ml-2'>
                     <h2 className='text-5xl sm:text-7xl font-bold'>I'm a full stack <br></br> Developer</h2>
                     <p className='text-gray py-4 max-w-md'>I'm a Freelance UI/UX Designer and Developer based in London, Engl and. I strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.</p>
                     <div>
