e5b79e04d2dcd74450ca0b3e0569767e97689b18
kimzux <kimzux0718@gmail.com>
2023-01-16 15:12:33 +0300

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,30 +30,30 @@ const About = () => {
                         <h3 class="mb-6 text-lg font-bold mt-0 xl:mt-0 2xl:mt-0 lg:mt-0 ">My Skills</h3>
                         <div class="flex flex-col space-y-4">
                             <div class="flex space-x-4">
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>HTML</span>
                                 </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>CSS</span>
                                 </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded"><span>php</span>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>php</span>
                                 </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded"><span>laravel</span>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>laravel</span>
                                 </div>
                             </div>
                             <div class="flex space-x-4">
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>JavaScript</span>
-                                </div><div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                </div><div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>React</span>
                                 </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded"><span>Git</span>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>Git</span>
                                 </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>GitHub</span></div></div><div class="flex space-x-4"><div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                         <span>Tailwind CSS</span>
                                     </div>
-                                <div class="flex items-center justify-center w-full px-3 py-3 bg-pincolor-900 text-gray-800 font-semibold rounded">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
                                     <span>Responsive Design</span>
                                 </div>
                             </div>
