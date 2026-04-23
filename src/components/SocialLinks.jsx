63b2755e76c1dc2408cde2182fb5b9d3735d146c
zulfa <56471192+kimzux@users.noreply.github.com>
2026-04-23 15:07:25 +0300

    Update resume link to new file name

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -43,7 +43,7 @@ const SocialLinks = () =>{
                Resume <BsFillPersonLinesFill size={30}/>
                </>
     ),
-    href:'/resume.pdf',
+    href:'/Zulfa-Rajabu Resume.pdf',
     style: 'rounded-br-md',
     download: true
 
